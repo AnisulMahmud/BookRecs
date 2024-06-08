ca4861f59fe8b8394df4eb5443ac5a7e931ce84f
Md. Aidul Islam <aidul5613@gmail.com>
2024-06-09 01:26:39 +0300

    books router done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const bookRoutes = require("./routers/book.router");
+
+//app.use("/api/books", bookRoutes);
+
+app.use("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT || 5001, () => {
-  console.log(`server running on ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 5001, () => {
+      console.log(`server running on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
